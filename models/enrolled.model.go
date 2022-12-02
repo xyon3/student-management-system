6188f09f6d8151dfd58bf47b65aa4f758e2dd0cf
xyon3 <reyespauldavid@gmail.com>
2022-12-01 23:37:28 -0500

    supplied gorm:column to fix the insertCourse() bug

--- models/enrolled.model.go
+++ models/enrolled.model.go
@@ -1,9 +1,15 @@
 package models
 
 type Enrolled struct {
-	StudID   string `json:"studID"`
-	CourseID string `json:"courseID"`
+	StudID   string `json:"studID" gorm:"column:studID"`
+	CourseID string `json:"courseID"  gorm:"column:courseID"`
 	ID       int64  `json:"id"`
 }
 
-func (Enrolled) TableName() string { return "juncEnrolled" }
+type EnrolledRequestBody struct {
+	StudID   string `json:"studID" gorm:"column:studID"`
+	CourseID string `json:"courseID" gorm:"column:courseID"`
+}
+
+func (Enrolled) TableName() string            { return "juncEnrolled" }
+func (EnrolledRequestBody) TableName() string { return "juncEnrolled" }
